a7b165b1e7abafdd3b795067374c65c20f060a5a
Nick Klauer <klauer@gmail.com>
2015-12-23 21:59:40 -0600

    somehow made it to the next level

--- cmd/chock_a_block/main.go
+++ cmd/chock_a_block/main.go
@@ -13,8 +13,6 @@ import (
 // goal: purchase 100,000 shares.  Not sure what the parameters are supposed to be, but as long as it doesn't hose me,
 // I think keeping the sells above the buys should keep me in the black.
 
-var lastAsk, lastBid int
-var diffBid, diffAsk int
 var myAsk, myBid int
 
 var book struct {
@@ -62,26 +60,6 @@ func main() {
 	fmt.Println("Tickers stopped")
 }
 
-// bookOrders will book+cancel orders based what's on the order book and where our current listing of bids is at
-func bookOrders(ticker *time.Ticker, level *s.Level) {
-	for range ticker.C {
-		mBid := book.orders.Bids[0].Price
-		for _, bid := range book.orders.Bids {
-			if bid.Price > mBid {
-				mBid = bid.Price
-			}
-		}
-		mAsk := book.orders.Asks[0].Price
-		for _, ask := range book.orders.Asks {
-			if ask.Price < mAsk {
-				mAsk = ask.Price
-			}
-		}
-		fmt.Printf("MaxBid: %d\tMinAsk: %d\n", mBid, mAsk)
-		//fmt.Printf("Depth - Asks: %-5d Bids: %-5d\n", len(book.orders.Asks), len(book.orders.Bids))
-	}
-}
-
 func printTickerTape(ws *websocket.Conn, level *s.Level) {
 	for {
 		var quote s.QuoteResponse
@@ -90,11 +68,14 @@ func printTickerTape(ws *websocket.Conn, level *s.Level) {
 			log.Println("read:", err)
 		}
 		// Create Quote, fix OrderBook
-		fmt.Printf("Spread: %4d / %-4d - Last %5d\t\n", quote.Quote.Bid, quote.Quote.Ask, quote.Quote.Last)
+		fmt.Printf("Spread: %5d / %-5d - Last %5d\t\n", quote.Quote.Bid, quote.Quote.Ask, quote.Quote.Last)
 
 		if quote.Quote.Bid > myBid && quote.Quote.Ask > myAsk {
 			diff := quote.Quote.Bid - myBid
-			if diff < 100 && diff > -100 {
+			if diff < 0 {
+				diff = diff * -1
+			}
+			if diff < 100 {
 				fmt.Printf("Difference is %d", diff)
 				myBid = quote.Quote.Last + 10
 			}
@@ -139,77 +120,5 @@ func printExecutions(ws *websocket.Conn, level *s.Level) {
 		}
 		// Cancel OrderBook, adjust bids
 		//fmt.Printf("Execution: %+v\n", execution)
-	}
-}
-
-func queryQuotes(ticker *time.Ticker, level *s.Level) {
-	for range ticker.C {
-		quote, err := c.GetQuote(level.Venues[0], level.Tickers[0])
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if quote.Ask > 0 {
-			diffAsk = quote.Ask - lastAsk
-			lastAsk = quote.Ask
-			//				changeAsk = changeAsk - lastAsk
-		}
-		if quote.Bid > 0 {
-			diffBid = quote.Bid - lastBid
-			lastBid = quote.Bid
-			//				changeBid = changeBid - lastBid
-		}
-		//			fmt.Printf("Spread: %4d (%5d) [%4d] / %-4d (%5d) [%4d]\tQuote: %s\tLast: %s\n", quote.Bid, diffBid, changeBid, quote.Ask, diffAsk, changeAsk, quote.QuoteTime, quote.LastTrade)
-		fmt.Printf("Spread: %4d (%5d) / %-4d (%5d)\t\n", quote.Bid, diffBid, quote.Ask, diffAsk)
-		//			price := calcPrice(quote)
-		//			order := &s.Order{
-		//				Account:   level.Account,
-		//				Venue:     VENUE,
-		//				Stock:     STOCK,
-		//				Qty:       100,
-		//				Direction: "buy",
-		//				OrderType: "limit",
-		//				Price:     calcBuy(quote),
-		//			}
-		//			result, err := s.PlaceOrder(order, os.Getenv(API_KEY_ENV))
-		//			if err != nil {
-		//				log.Fatalf("Error: %v\nResponse: %v", err, result)
-		//			}
-		//			order.Direction = "sell"
-		//			order.Qty = 75
-		//			order.Price = calcSell(quote)
-		//			result, err = s.PlaceOrder(order, os.Getenv(API_KEY_ENV))
-	}
-}
-
-func calcBuy(quote *s.Quote) int {
-	if quote.Ask > 0 {
-		if quote.Bid > 0 {
-			return quote.Bid - 10
-		} else {
-			return quote.Last + 10
-		}
-	} else {
-		if quote.Bid > 0 {
-			return quote.Bid - 10
-		} else {
-			return quote.Last + 10
-		}
-	}
-}
-
-func calcSell(quote *s.Quote) int {
-	if quote.Ask > 0 {
-		if quote.Bid > 0 {
-			return quote.Bid + 10
-		} else {
-			return quote.Last - 10
-		}
-	} else {
-		if quote.Bid > 0 {
-			return quote.Bid + 10
-		} else {
-			return quote.Last - 10
-		}
 	}
 }
